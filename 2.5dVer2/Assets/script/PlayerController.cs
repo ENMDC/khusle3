a977900ddfe693e071de803ed1c0acf95bdaab91
enkhkhuslen7 <161922912+ENMDC@users.noreply.github.com>
2026-01-26 12:30:33 +0900

    movement

--- 2.5dVer2/Assets/script/PlayerController.cs
+++ 2.5dVer2/Assets/script/PlayerController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public float jumpForce;
     private Rigidbody rb;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,11 +14,51 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        float y = Input .GetAxis("JetPackMovement");
-        Vector3 moveDir = new Vector3(x, y, z);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if ( Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0,-50f * Time.deltaTime,0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, 50f * Time.deltaTime, 0);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Rotate(50f * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Rotate(-50f * Time.deltaTime, 0,0 );
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate( 0, 0,-speed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(0,  0,speed * Time.deltaTime);
+            }
+            float y = Input .GetAxis("JetPackMovement");
+        Vector3 moveDir = new Vector3(0, -y, 0);
         rb.linearVelocity = moveDir * speed;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
 
     }
 }
