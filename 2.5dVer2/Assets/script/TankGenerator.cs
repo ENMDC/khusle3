e201ebd02deb8a66390b841dcb1f967c9cedd9cb
sokoyu <161922912+ENMDC@users.noreply.github.com>
2026-05-26 12:29:45 +0900

    dash start
    just create some of float that used for dash and tsegtselsen

--- 2.5dVer2/Assets/script/TankGenerator.cs
+++ 2.5dVer2/Assets/script/TankGenerator.cs
@@ -11,7 +11,7 @@ public class TankGenerator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        CreateTank(5);
+        CreateTank(8);
     }
     void CreateTank(int num)
     {
--- 2.5dVer2/Assets/script/enemy.cs
+++ 2.5dVer2/Assets/script/enemy.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject Tank;
     public int damage = 10;
     public float health = 100f;
 
@@ -14,10 +15,16 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
+    private void spawItem()
+    {
+        Vector3 spawnItem = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        GameObject TankClone = Instantiate(Tank, spawnItem, Quaternion.identity);
+    }
 
     void Die()
     {
         Destroy(gameObject);
+        spawItem();
     }
 
     private void OnTriggerEnter(Collider other)
